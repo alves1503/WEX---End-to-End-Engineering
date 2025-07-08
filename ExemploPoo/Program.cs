c0684c999c76660928fa2636133eec21c684ca22
alves1503 <henriquefdalves15@gmail.com>
2025-07-08 00:00:32 -0300

    Iniciei os estudos com POO em C#

---
+++ ExemploPoo/Program.cs
@@ -0,0 +1,22 @@
+﻿using ExemploPoo.models;
+
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+
+
+
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
+
+
+
+
+
+
+/* Pessoa p1 = new Pessoa();
+p1.Nome = "Henrique";
+p1.Idade = 20;
+
+p1.Apresentar(); */
---
+++ ExemploPoo/models/Pessoa.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPoo.models
+{
+    public class Pessoa
+    {
+
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Ola meu nome é {Nome} e tenho {Idade} anos");
+        }
+
+
+    }
+}
